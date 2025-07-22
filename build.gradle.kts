11c09b9155cf748bc4b55b7aac9c6ea7fbdc8894
YKDZ <3070799584@qq.com>
2025-07-22 18:23:54 +0800

    [0.4.1-Beta] Bump paper api 1.21.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     project.group = "cn.encmys.ykdz.forest"
-    project.version = "0.4.0-Beta"
+    project.version = "0.4.1-Beta"
 
     apply<JavaPlugin>()
     apply(plugin = "java")
