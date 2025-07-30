47ebfe350b7575cdc3848265256b36e594e8c5a5
YKDZ <3070799584@qq.com>
2025-07-30 08:22:41 +0800

    [0.4.3-Beta] API version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     project.group = "cn.encmys.ykdz.forest"
-    project.version = "0.4.2-Beta"
+    project.version = "0.4.3-Beta"
 
     apply<JavaPlugin>()
     apply(plugin = "java")
