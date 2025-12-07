420292b9b0f6fedc0a28d7842ab3e7b6572c368a
YKDZ <3070799584@qq.com>
2025-12-07 11:37:41 +0800

    v0.5.0-Beta: release

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     project.group = "cn.encmys.ykdz.forest"
-    project.version = "0.4.7-Beta"
+    project.version = "0.5.0-Beta"
 
     apply<JavaPlugin>()
     apply(plugin = "java")
